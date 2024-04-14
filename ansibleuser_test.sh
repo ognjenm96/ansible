d95beaf502dbdad7b88e7016a7aaedba4d20e146
ognjenm96 <112555599+ognjenm96@users.noreply.github.com>
2024-04-14 10:32:28 +0200

    Update ansibleuser_test.sh

--- ansibleuser_test.sh
+++ ansibleuser_test.sh
@@ -1,8 +1,8 @@
 /bin/bas
 # Kreiranje ansible korisnika za adminsitraciju kroz ansible
 #Kreiranje korisnika ansible
-sudo useradd ansibletest # Promeniti ime i finalnoj verziji
+sudo useradd ansibledev # Promeniti ime i finalnoj verziji
 #Dodavanje usera u odgovarajuce grupe
-sudo usermod -a -G wheel ansibletest # Promeniti ime i finalnoj verziji
+sudo usermod -a -G wheel ansibledev # Promeniti ime i finalnoj verziji
 #Postavljane sifre za usera
-sudo passwd ansibletest # Promeniti ime i finalnoj verziji
+sudo passwd ansibledev # Promeniti ime i finalnoj verziji
