31a35976655a2058c3223690600f1900a4963b7b
“Ognjen” <ognjen.mitic.mes@gmail.com>
2024-06-12 14:03:26 +0200

    changes

--- data_migration-old.sh
+++ data_migration-old.sh
@@ -1,36 +1,27 @@
-server_ip_new = 172.21.0.30
-user = six
-password = Six4Me
-
-sudo systemctl stop slapd
-sudo systemctl stop postfix 
-sudo systemctl stop ntpsec 
-sudo systemctl stop bind9 
-sudo systemctl stop munin-node 
-sudo systemctl stop isc-dhcp-server
-
-echo 'Transfer OpenLDAP configuration and data:'
+server_ip_new=172.16.2.142
+user=six
+password=Six4Me
 
 sudo slapcat -v -l /home/six/ldap_backup.ldif
 scp /home/six/ldap_backup.ldif $user@$server_ip_new:/home/$user/ldap_backup.ldif
-sudo rsync -avz  /etc/ldap/ $user@$server_ip_new:/six/ldap/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/ldap/ $user@$server_ip_new:/home/$user/
 
-exho 'Transfer BIND configuration and zone files: '
 
-sudo rsync -avz  /etc/bind/ $user@$server_ip_new:/six/bind/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/bind/ $user@$server_ip_new:/home/$user/
 
-echo 'Transfer DHCP server configuration: '
-
-sudo rsync -avz  /etc/dhcp/ $user@$server_ip_new:/six/dhcp/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/dhcp/ $user@$server_ip_new:/home/$user/
 
 echo 'Transfer NTPsec configuration: '
-
-sudo rsync -avz  /etc/ntpsec/ $user@$server_ip_new:/six/ntpsec/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/ntpsec/ $user@$server_ip_new:/home/$user/
 
 echo 'Transfer Postfix configuration: '
-
-sudo rsync -avz  /etc/postfix/ $user@$server_ip_new:/six/postfix/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/postfix/ $user@$server_ip_new:/home/$user/
 
 echo 'Transfer Munin configuration: '
-
-sudo rsync -avz  /etc/munin/ $user@$server_ip_new:/six/munin/
+read -s password
+sudo rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" /etc/munin/ $user@$server_ip_new:/home/$user/
